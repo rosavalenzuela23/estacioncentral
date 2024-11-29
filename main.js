586741258d0622d20263a4e1189c82dcb047d8f9
rosa <natsukiarr@gmail.com>
2024-11-28 17:07:10 -0700

    CHANGE: ws to wss

--- main.js
+++ main.js
@@ -6,10 +6,19 @@ const createWindow = () => {
         width: 800,
         height: 600,
         webPreferences: {
-            sandbox: false, //La neta este codigo me da miedo.... 
+            sandbox: false, //La verdad este codigo me da miedo.... 
             preload: path.join(__dirname, 'dist/main.js')
         }
     });
+
+    win.webContents.session.setCertificateVerifyProc((request, callback) => {
+        const { hostname } = request
+        if (hostname === 'localhost') { // Este es el host del cual el navegador de electron va a hace la vista de lado
+          callback(0); 
+        } else {
+          callback(-3); //este es para rechazar el CA autofirmado si es que no esta en el localhost
+        }
+      })
 
     win.loadFile('./dist/index.html');
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+const fs = require('fs');
 const Stomp = require('stompjs');
 import { on_connect, on_error } from './conexiones';
 
-
-const url = "ws://localhost:15674/ws"
+const url = "wss://localhost:15673/ws"
 
 const ws = new WebSocket(url);
 const client = Stomp.over(ws);
